ff57852df961600313f1dc259bb01b657f8fc176
Nick Moore <nicholas.i.moore@verizondigitalmedia.com>
2017-09-22 13:09:00 -0700

    update test_index.py

--- test_index.py
+++ test_index.py
@@ -2,57 +2,52 @@
 import requests
 import json
 import sys
+from log_parser import time_helper
+from log_parser import parse_helper
+t = time_helper.time_helper()
+p = parse_helper.parse_helper()
 
 url = 'http://ripe.corp.vdms.io/data/processed/results-tbm-1046.json'
-list_one = []
 
-def get_prb(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            for item in data:
-                if 'prb_id' in item:
-                    list_one.append('PROBE ID :', item['prb_id'])
-        except:
-            pass
+class store:
+    def __init__(self):
+        self.data = {}
 
-def get_rtt(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            for item in data:
-                if 'result' in item and 'rt' in item['result']:
-                    print'RTT :', item['result']['rt']
+    def add_the_things(self, dns_server,  rtt, asn, probeID, timestamp):
+        print rtt
+        self.data[‘asn’] = asn
+        self.data[‘dns_server’] = dns_server
+        self.data[‘rtt’] = rtt
+        self.data[‘probeID’] = probeID
+        self.data[‘timestamp’] = t.to_sdi(timestamp)
+        p.write_json(‘file’, self.data)
 
-        except:
-            pass
+    def return_data():
+        return self.data
 
-def get_asn(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            for item in data:
-                if 'asn_v4' in item:
-                    print'ASN :', item['asn_v4']
-        except:
-            pass
+s = store()
 
-def get_src(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            for item in data:
-                if 'from' in item:
-                    print'SOURCE :', item['from']
-        except:
-            pass
+if __name__ == ‘__main__‘:
 
 
-print list_one
-print get_src(url)
-print get_asn(url)
-print get_rtt(url)
+    def get_the_things(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            try:
+                for item in data:
+                    timestamp = item.get(‘timestamp’, 0)
+                    probeID = item.get(‘prb_id’)
+                    asn = item.get(‘asn_v4’)
+                    _d = item.get(‘result’)
+                    rtt = _d.get(‘rt’)
+                    answers = _d.get(‘answers’)
+                    for _x in answers:
+                        dns_server = _x.get(‘RDATA’)
+                    #print dns_server, rtt, asn, probeID, timestamp
+                    s.add_the_things(dns_server, rtt, asn, probeID, timestamp)
+            except:
+                pass
+    get_the_things(url)
+
+    s.return_data
