a158c5f191952a4380247d8ba4d903c632b1c5a5
Nick Moore <nicholas.i.moore@verizondigitalmedia.com>
2017-10-02 12:43:01 -0700

    update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,53 +1,33 @@
-#!/usr/bin/env python
-import requests
-import json
-import sys
-from log_parser import time_helper
-from log_parser import parse_helper
-t = time_helper.time_helper()
-p = parse_helper.parse_helper()
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-url = 'http://ripe.corp.vdms.io/data/processed/results-tbm-1046.json'
-
-class store:
-    def __init__(self):
-        self.data = {}
-
-    def add_the_things(self, dns_server,  rtt, asn, probeID, timestamp):
-        print rtt
-        self.data[‘asn’] = asn
-        self.data[‘dns_server’] = dns_server
-        self.data[‘rtt’] = rtt
-        self.data[‘probeID’] = probeID
-        self.data[‘timestamp’] = t.to_sdi(timestamp)
-        p.write_json(‘file’, self.data)
-
-    def return_data():
-        return self.data
-
-s = store()
-
-if __name__ == ‘__main__‘:
+from collections import defaultdict
+import os, gzip, json
+import os.path
 
 
-    def get_the_things(url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            try:
-                for item in data:
-                    timestamp = item.get(‘timestamp’, 0)
-                    probeID = item.get(‘prb_id’)
-                    asn = item.get(‘asn_v4’)
-                    _d = item.get(‘result’)
-                    rtt = _d.get(‘rt’)
-                    answers = _d.get(‘answers’)
-                    for _x in answers:
-                        dns_server = _x.get(‘RDATA’)
-                    #print dns_server, rtt, asn, probeID, timestamp
-                    s.add_the_things(dns_server, rtt, asn, probeID, timestamp)
-            except:
-                pass
-    get_the_things(url)
 
-    s.return_data
+root = '/Users/nimoore/'
+dirs=['results-tbm-1046.json']
+data={}
+
+def test_index(dirs):
+    for f in dirs:
+        filePath = root + f
+        print filePath
+        if f.startswith('results-tbm'):
+            with open(filePath, "r") as myfile:
+                for line in myfile:
+                    js = json.loads(line)
+                    for i in js:
+                        if 'prb_id' in i and 'result' in i and 'answers' in i['result'].keys():
+                            data={}
+                            data['prb_id'] = i['prb_id']
+                            data['timestamp']=i['timestamp']
+                            data['asn']=i['asn_v4']
+                            data['pop']=dict(zip(i['result']['answers'][0].keys(), i['result']['answers'][0].values()))['RDATA'][0]
+                            data['rt']=i['result']['rt']
+                            with open('/Users/nimoore/data.json', 'a') as outfile:
+                                json.dump(data, outfile)
+                                outfile.write("\n")
+    return
